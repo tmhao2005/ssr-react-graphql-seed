04f6fa5612b6c6bd3164e27da5c5e4ffb7bbfddf
Hao Tran <hao.tran@go1.com>
2020-05-15 00:07:12 +0700

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 ### Objective
 
 Try to provide the seed project for everyone can understand server side rendering (SSR) with React + GraphQL
+
+### Start
+
+```sh
+yarn 
+yarn dev
+```
+
+### Build
+
+```sh
+yarn build
+```
--- tools/dev.ts
+++ tools/dev.ts
@@ -1,4 +1,3 @@
-import path from "path";
 import express from "express";
 import webpack from "webpack";
 import webpackDevMiddleware from "webpack-dev-middleware";
@@ -16,25 +15,16 @@ import { common } from "./global";
 let server: express.Application;
 
 async function start() {
-  if (server)
-    return server;
-
   // Build a dev server
   server = express();
-
-  server.use(express.static(path.resolve(__dirname, "../public")));
 
   clientConfig.output.filename = (clientConfig.output.filename as string).replace("chunkhash", "hash");
   clientConfig.output.chunkFilename = clientConfig.output.chunkFilename.replace("chunkhash", "hash");
   clientConfig.module.rules = clientConfig.module.rules.filter(x => x.loader !== "null-loader");
-  clientConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
 
   serverConfig.output.hotUpdateMainFilename = "updates/[hash].hot-update.json";
   serverConfig.output.hotUpdateChunkFilename = "updates/[id].[hash].hot-update.js";
   serverConfig.module.rules = serverConfig.module.rules.filter(x => x.loader !== "null-loader");
-  serverConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
-
-  // await run(clean);
 
   const multiCompiler = webpack([clientConfig, serverConfig]);
 
